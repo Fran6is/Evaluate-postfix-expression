3fde2e2a2eee7bf855c6ea31173395e784afe002
Learner <francislarge66@gmail.com>
2019-12-09 12:17:50 -0600

    Updates

---
+++ precache-manifest.eca8a3e93be31ff9cb18a778c8434ee6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "968624d78381b4ed68d2468ce1999a22",
+    "url": "/Postfix-exp/index.html"
+  },
+  {
+    "revision": "63a55b39fff438cf49f1",
+    "url": "/Postfix-exp/static/css/main.f567a4ac.chunk.css"
+  },
+  {
+    "revision": "6c999f83a060ab7e75a4",
+    "url": "/Postfix-exp/static/js/2.7a1c26f3.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/Postfix-exp/static/js/2.7a1c26f3.chunk.js.LICENSE"
+  },
+  {
+    "revision": "63a55b39fff438cf49f1",
+    "url": "/Postfix-exp/static/js/main.a3b60eab.chunk.js"
+  },
+  {
+    "revision": "251d91cbeda551825455",
+    "url": "/Postfix-exp/static/js/runtime-main.72cc9743.js"
+  }
+]);
